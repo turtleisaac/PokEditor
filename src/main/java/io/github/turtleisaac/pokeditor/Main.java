df6623ea45c443dc1b5bdf5ae5686bb60bb08344
turtleisaac <turtleisaac1@gmail.com>
2023-11-12 20:14:34 -0600

    adjusted main to put themes in different order

--- src/main/java/io/github/turtleisaac/pokeditor/Main.java
+++ src/main/java/io/github/turtleisaac/pokeditor/Main.java
@@ -1,23 +1,13 @@
 package io.github.turtleisaac.pokeditor;
 
-import javax.swing.*;
-
-import com.formdev.flatlaf.extras.FlatUIDefaultsInspector;
 import com.formdev.flatlaf.intellijthemes.*;
-import com.formdev.flatlaf.intellijthemes.materialthemeuilite.FlatAtomOneLightIJTheme;
-import com.formdev.flatlaf.intellijthemes.materialthemeuilite.FlatLightOwlIJTheme;
-import com.formdev.flatlaf.intellijthemes.materialthemeuilite.FlatMaterialLighterIJTheme;
-import com.formdev.flatlaf.intellijthemes.materialthemeuilite.FlatSolarizedLightIJTheme;
 import io.github.turtleisaac.nds4j.ui.ProgramType;
 import io.github.turtleisaac.nds4j.ui.Tool;
-import io.github.turtleisaac.nds4j.ui.exceptions.ToolCreationException;
 import io.github.turtleisaac.pokeditor.gui.PokeditorManager;
 import io.github.turtleisaac.pokeditor.gui_old.projects.projectwindow.console.ConsoleWindow;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
-import java.util.Locale;
-import java.util.function.Supplier;
 
 /**
  * @author turtleisaac
@@ -31,7 +21,6 @@ public class Main
 
     public static void main(String[] args) throws IOException
     {
-        FlatUIDefaultsInspector.install( "ctrl shift alt Y" );
         String[] mainMenuJokes = new String(Main.class.getResourceAsStream(jokesPath).readAllBytes(), StandardCharsets.UTF_8).split("\n");
 
 //        Locale.setDefault(Locale.CHINA);
@@ -42,8 +31,8 @@ public class Main
 //                .setFlavorText("Did you know that Jay likes Moemon?")
                 .setFlavorText(mainMenuJokes[(int) (Math.random()*(mainMenuJokes.length))])
                 .setAuthor("Developed by Turtleisaac")
-                .addLookAndFeel(new FlatDarkPurpleIJTheme())
                 .addLookAndFeel(new FlatArcOrangeIJTheme())
+                .addLookAndFeel(new FlatDarkPurpleIJTheme())
 //                .addLookAndFeel(new javax.swing.plaf.metal.MetalLookAndFeel())
                 .addGame("Pokémon Platinum", "CPU")
                 .addGame("Pokémon HeartGold","IPK")
